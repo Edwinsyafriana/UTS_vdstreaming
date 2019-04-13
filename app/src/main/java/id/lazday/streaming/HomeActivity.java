46279b4ce19e42330a55ffe63fddf5cb451d868a
Edwinsyafriana <edwinsyafri99@yahoo.com>
2019-04-13 10:27:43 +0700

    first commit

---
+++ app/src/main/java/id/lazday/streaming/HomeActivity.java
@@ -0,0 +1,73 @@
+package id.lazday.streaming;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import id.lazday.streaming.activity.MainActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    SharedPreferences pref; //Declaration SharedPreferences
+    Cursor cursor; //Declaration Cursor
+    DataHelper dataHelper; //Declaration SqliteHelper
+    TextView txtusername; //Declaration Textview
+    Button buttonLogout; //Declaration Button
+    Button buttonStreaming;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        pref = getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+        String session_id =  pref.getString("session",null);
+
+        dataHelper = new DataHelper(this);
+        // Query check id user
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        cursor = db.rawQuery("SELECT username FROM users WHERE id = '" + session_id + "'",null);
+        cursor.moveToFirst();
+        if (cursor.getCount()>0) {
+            TextView txtusername = (TextView) findViewById(R.id.txtusername);
+            txtusername.setText("Selamat Datang, "+cursor.getString(0).toString());
+        }
+
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedpreferences = getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.clear();
+                editor.commit();
+                //User Logged in Successfully Launch You home screen activity
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        buttonStreaming = (Button) findViewById(R.id.buttonStreaming);
+        buttonStreaming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedpreferences = getSharedPreferences("SharedPref", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = pref.edit();
+                editor.clear();
+                editor.commit();
+                //User Logged in Successfully Launch You home screen activity
+                Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+}
